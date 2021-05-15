ee0e46b68a75095f957c6ec47a4da414b7fbfd73
Alexa-Malina <78047357+Alexa-Malina@users.noreply.github.com>
2021-05-15 15:31:04 +0200

    Update app.R
    rename H,
    remove old code of Kate for testing purposes

--- App/app.R
+++ App/app.R
@@ -16,6 +16,16 @@ library(dplyr)
 
 
 
+
+
+
+
+
+
+
+
+
+
 library(tidyverse)
 library(magrittr)
 library(lubridate)
@@ -68,32 +78,6 @@ ui<- fluidPage(
                  mainPanel()
                )
       ),
-      tabPanel("Delayed Testing", fluid = TRUE,
-               sidebarLayout(
-                 sidebarPanel(checkboxGroupInput("ages2", label=h3("Select Age Group"),
-                                                 choices = list('0-19','20-39','40-59','60-79','80+'),
-                                                 selected = '0-19'),
-                              checkboxGroupInput('traveled', label=h3("Previous Travel?"),
-                                                 choices = list('yes', 'no'),
-                                                 selected = 'yes'),
-                              checkboxGroupInput('contact', label=h3("Known Contact?"),
-                                                 choices = list('yes', 'no', 'unknown'),
-                                                 selected = 'yes'), 
-                              checkboxGroupInput('sex', label=h3("Select Sex"),
-                                                 choices = list('male', 'female'),
-                                                 selected = 'male')
-                 ),
-                 mainPanel(
-                   tabsetPanel(
-                     tabPanel("By Age", plotOutput('delayed')),
-                     tabPanel("Previous Travel", plotOutput('delayed1')),
-                     tabPanel("Known Contact", plotOutput('delayed2')),
-                     tabPanel("By Sex", plotOutput('delayed3'))
-                   )
-                 )
-               )
-      ),
-      
       
       
       tabPanel("E", fluid = TRUE,
@@ -315,7 +299,7 @@ ui<- fluidPage(
       ),
       
       
-      tabPanel("H", fluid = TRUE,
+      tabPanel("H - predition testing", fluid = TRUE,
                sidebarLayout(
                  sidebarPanel(),
                  mainPanel(
@@ -389,82 +373,6 @@ server <- function(input, output) {
     ####
     
     #### Tab D
-    
-    ####
-    
-    #### Tab "Delayed Testing" 
-    
-    # Density curves of days until testing
-    
-    # By Age Category 
-    
-    output$delayed <- renderPlot({
-      CalwData2 <- 
-        CalwData %>% 
-        mutate(delayed.testing = ymd(indexcase)- symptoms.onsetDate) %>%
-        
-        mutate(Age_Cat = ifelse(age == '0-4' | age == '5-9' | age == '10-14' | age == '15-19', 1,
-                                ifelse(age == '20-24' | age == '25-29' | age == '30-34' | age == '35-39', 2,
-                                       ifelse(age == '40-44' | age == '45-49' | age == '50-54' | age == '55-59', 3,
-                                              ifelse(age == '60-64' | age == '65-69' | age == '70-74' | age == '75-79', 4, 5))))) 
-      
-      CalwData2$Age_Cat <- factor(CalwData2$Age_Cat,
-                                  levels = c(1,2,3, 4, 5),
-                                  labels = c("0-19", "20-39", "40-59", "60-79", "80+"))
-      
-      
-      
-      
-      ggplot(CalwData2[CalwData2$Age_Cat %in% input$ages2,], aes(delayed.testing, fill = Age_Cat))+geom_density(alpha=0.4) + xlim(c(-10, 14)) + theme_minimal() + scale_fill_brewer(palette="Accent")
-      
-    })
-    
-    # By Traveled Status 
-    
-    output$delayed1 <- renderPlot({
-      
-      CalwData2 <- 
-        CalwData %>% 
-        mutate(delayed.testing = ymd(indexcase)- symptoms.onsetDate)
-      
-      
-      ggplot(CalwData2[CalwData2$traveled %in% input$traveled,], aes(delayed.testing, fill = traveled))+geom_density(alpha=0.4) + xlim(c(-5, 10)) + theme_minimal() + scale_fill_brewer(palette="Dark2")
-    })
-    
-    # By Known Contact Status 
-    
-    output$delayed2 <- renderPlot({
-      CalwData2 <- 
-        CalwData %>% 
-        mutate(delayed.testing = ymd(indexcase)- symptoms.onsetDate)
-      
-      ggplot(CalwData2[CalwData2$contactSourceCase %in% input$contact,], aes(delayed.testing, fill = contactSourceCase))+geom_density(alpha=0.4) + xlim(c(-5, 10)) + theme_minimal() + scale_fill_brewer(palette="Dark2")
-    })
-    
-    #By Sex 
-    
-    output$delayed3 <- renderPlot({
-      CalwData2 <- 
-        CalwData %>% 
-        mutate(delayed.testing = ymd(indexcase)- symptoms.onsetDate)
-      
-      mu <- CalwData2 %>% 
-        group_by(sex) %>% 
-        summarize (grp.mean = mean(delayed.testing, na.rm = TRUE))
-      
-      
-      ggplot(CalwData2[CalwData2$sex %in% input$sex,], aes(delayed.testing, fill = sex))+geom_density(alpha = 0.4) + geom_vline(mu[mu$sex %in% input$sex,], xintercept = mu$grp.mean) + xlim(c(-5, 10)) + theme_minimal()+ scale_fill_brewer(palette="Dark2")
-    })
-    
-    # By Pregnancy Status 
-    
-    output$delayed4 <- renderPlot({
-      CalwData2 <- 
-        CalwData %>% 
-        mutate(delayed.testing = ymd(indexcase)- symptoms.onsetDate)
-      
-      ggplot(CalwData2[CalwData2$pregnant %in% input$pregnant,], aes(delayed.testing, fill = pregnant))+geom_density(alpha = 0.4) + xlim(c(-5, 10)) + theme_minimal()+ scale_fill_brewer(palette="Dark2")
-    })
     
     ####
     
