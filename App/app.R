c08012efa64c966a30ef2de432f8e7cdfb14b402
Lawreros <lawreros@gmail.com>
2021-05-16 16:52:50 -0400

    add death and case numbers

--- App/app.R
+++ App/app.R
@@ -44,7 +44,10 @@ ui<- fluidPage(
                                min = as.Date("2020-03-04","%Y-%m-%d"),
                                max = as.Date("2021-05-11","%Y-%m-%d"),
                                value=as.Date("2020-03-20"),
-                               timeFormat="%Y-%m-%d")
+                               timeFormat="%Y-%m-%d"),
+                   radioButtons("radio", label = h3("Radio buttons"),
+                                choices = list("Choice 1" = 'case', "Choice 2" = 'death'), 
+                                selected = 'case')
                  ),
                  mainPanel(
                    leafletOutput('covid_map'),
@@ -163,16 +166,26 @@ server <- function(input, output) {
     pal <- colorNumeric("viridis", NULL)
     nycounties <- rgdal::readOGR("DE-counties.geojson")
     
-    dat <- read.csv('7di-rl-by-ags.csv')
+    c_dat <- read.csv('cases-rki-by-ags.csv')
+    c_dat <- c_dat %>% rename(sum = sum_cases)
+    c_dat$category <- 'case'
+    d_dat <- read.csv('deaths-rki-by-ags.csv')
+    d_dat$category <- 'death'
+    d_dat <- d_dat %>% rename(sum = sum_deaths)
+    
+    dat <- rbind(c_dat,d_dat)
+    dat <- dat[,c(1:325,414,338:413,416)]
+    
     dat$time_iso8601 <- as.Date(dat$time_iso8601,"%Y-%m-%dT")
-    new_name <- paste0(nycounties@data$GEN, " (", nycounties@data$BEZ, ")")
-    colnames(dat) <- append('time', c(new_name[1:401],'germany'))
+    new_name <- paste0(make.names(nycounties@data$GEN), "_", make.names(nycounties@data$BEZ), "")
+    new_name <- append('time', c(new_name[1:400],'germany','category'))
+    colnames(dat) <- new_name
     
     
     output$covid_map <- renderLeaflet({leaflet(nycounties)%>%
                           addTiles()%>%
                           addPolygons(stroke = FALSE, smoothFactor = 0.3, fillOpacity = 0.7,
-                              fillColor = pal(unlist(dat[dat$time == input$map_date,-1],use.names = FALSE)),
+                              fillColor = pal(unlist(with(dat,dat[(dat$time == input$map_date & dat$category == input$radio),-c(1,402,403)]),use.names = FALSE)),
                               # Highlight neighbourhoods upon mouseover
                               highlight = highlightOptions(
                               weight = 30,
@@ -182,18 +195,22 @@ server <- function(input, output) {
                               bringToFront = TRUE),
                               #sendToBack = TRUE), 
                               
-                              label = ~paste0(GEN, ": ", formatC(unlist(dat[dat$time == input$map_date,-1],use.names = FALSE), big.mark = ",")),layerId = seq.int(2,432)) %>%
-                          addLegend(pal = pal, title='new cases per 100k in last 7 days',values = ~log10(unlist(dat[dat$time == input$map_date,-1],use.names = FALSE)+1), opacity = 1.0,
+                              label = ~paste0(GEN, ": ", formatC(unlist(with(dat,dat[(dat$time == input$map_date & dat$category == input$radio),-c(1,402,403)])), big.mark = ",")),layerId = seq.int(2,402)) %>%
+                          addLegend(pal = pal, title='new cases per 100k in last 7 days',values = ~log10(unlist(with(dat,dat[(dat$time == input$map_date & dat$category == input$radio),-c(1,402,403)]),use.names = FALSE)+1), opacity = 1.0,
                               labFormat = labelFormat(transform = function(x) round(10^x)))
     }) #, layerId = nycounties@data$GEN
     
     observeEvent(input$covid_map_shape_click, { # update the location selectInput on map clicks
       p <- input$covid_map_shape_click
-      output$testthis <- renderText(dat[dat$time == input$map_date,p$id])
+      output$testthis <- renderText(p$id)
       
       output$countytime <- renderPlot({
-         ggplot(dat, aes(dat[,1], dat[,new_name[p$id]])) + 
+         ggplot(dat, aes_string("time", new_name[p$id], colour = "category")) + 
           geom_point()+
+          # scale_y_continuous(
+          #   trans = "log10",
+          #   breaks = 1:10
+          # )+
           geom_vline(xintercept = input$map_date, linetype="dotted")
       })
       
